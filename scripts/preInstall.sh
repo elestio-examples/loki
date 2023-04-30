3b0e936b28bb58c1e096ec9a2ed59815f4a70132
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-04-30 14:05:48 +0300

    fix

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -3,12 +3,6 @@ set -o allexport; source .env; set +o allexport;
 
 # apt install jq -y
 
-# mkdir -p ./crux-db
-# mkdir -p ./kratos-data
-# mkdir -p ./kratos-db
+mkdir -p ./data
 
-# chown -R 1000:1000 ./crux-db
-# chown -R 1000:1000 ./kratos-data
-# chown -R 1000:1000 ./kratos-db
-
-# sed -i "s|EMAIL_TO_CHANGE|${SMTP_FROM_EMAIL}|g" ./docker-compose.yml
+chown -R 1000:1000 ./data
